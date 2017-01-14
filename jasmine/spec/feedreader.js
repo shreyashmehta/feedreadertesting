0044a0f09193ba73c90601f668f843929269d54a
Shreyash Mehta <shreyash.p.mehta@gmail.com>
2017-01-14 15:43:34 +0530

    Update feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,7 +1,26 @@
+/* feedreader.js
+ *
+ * This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against your application.
+ */
 
+/* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
+ */
 $(function() {
+    /* This is our first test suite - a test suite just contains
+    * a related set of tests. This suite is all about the RSS
+    * feeds definitions, the allFeeds variable in our application.
+    */
     describe('RSS Feeds', function() {
-      
+        /* This is our first test - it tests to make sure that the
+         * allFeeds variable has been defined and that it is not
+         * empty. Experiment with this before you get started on
+         * the rest of this project. What happens when you change
+         * allFeeds in app.js to be an empty array and refresh the
+         * page?
+         */
         it('are defined', function() {
             //checks if allFeeds is defined
             expect(allFeeds).toBeDefined();
@@ -9,7 +28,7 @@ $(function() {
         });
 
 
-        /* Test that loops through each feed
+        /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -22,7 +41,7 @@ $(function() {
             });
         });
 
-        /* Test that loops through each feed
+        /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -37,19 +56,22 @@ $(function() {
     });
 
 
-    /* Test suite named "The menu" */
+    /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
 
-        /* Test that ensures the menu element is
-         * hidden by default. 
+        /* TODO: Write a test that ensures the menu element is
+         * hidden by default. You'll have to analyze the HTML and
+         * the CSS to determine how we're performing the
+         * hiding/showing of the menu element.
          */
+
         it('is hidden by default', function(){
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
-         /* Test that ensures the menu changes
+         /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
-          * has two expectations: does the menu display when
+          * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
 
@@ -66,16 +88,18 @@ $(function() {
           });
     });
 
-    /* Test suite named "Initial Entries" */
+    /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
 
         beforeEach(function(done) {
             loadFeed(0, done);
             });
         });
-        /* Test that ensures when the loadFeed
+        /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
+         * Remember, loadFeed() is asynchronous so this test will require
+         * the use of Jasmine's beforeEach and asynchronous done() function.
          */
          it('loadFeed Entries ', function() {
             //expects the length of entries not to br zero
@@ -84,10 +108,10 @@ $(function() {
 
     });
 
-    /* Test suite named "New Feed Selection"*/
+    /* TODO: Write a new test suite named "New Feed Selection"*/
     describe('New Feed Selection', function() {
 
-        /* Test that ensures when a new feed is loaded
+        /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
